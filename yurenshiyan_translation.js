7829cc3e73f42a109244572a6065eb53e5b0bf7d
ctweehaur <ctweehaur@gmail.com>
2026-06-02 16:58:10 +0800

    Create yurenshiyan_translation.js

---
+++ yurenshiyan_translation.js
@@ -0,0 +1,17 @@
+const translationData = [
+    {
+        zh: "从前有一个愚蠢的人，到朋友家做客。主人留他吃饭，他嫌弃饭菜太清淡，没有任何滋味。",
+        py: "xī qián yǒu yī gè yú chǔn de rén dào péng yǒu jiā zuò kè zhǔ rén liú tā chī fàn tā xián qì fàn cài tài qīng dàn méi yǒu rèn hé zī wèi",
+        en: "Once upon a time, there was a foolish man who went to visit someone's house. The host offered him food, but he complained that it was too bland and completely tasteless."
+    },
+    {
+        zh: "主人听了他的话后，特地为发他重新添加了些食盐。等到放了盐的饭菜变得味道鲜美后，傻子便在心里暗自琢磨：“这饭菜之所以美味，是因为加了盐的缘故。少放一点盐尚且如此美味，更何况放很多盐呢？”",
+        py: "zhǔ rén tīng le tā de huà hòu tè dì wèi fā tā chóng xīn tiān jiā le xiē shí yán děng dào fàng le yán de fàn cài biàn dé wèi dào xiān měi hòu shǎ zǐ biàn zài xīn lǐ àn zì zhuó mó zhè fàn cài zhī suǒ yǐ měi wèi ?? yīn wèi jiā le yán de yuán gù shǎo fàng yī diǎn yán shàng qiě rú cǐ měi wèi gèng kuàng fàng hěn duō yán ne",
+        en: "Hearing this, the host specifically added more salt for him. After experiencing the delicious taste, the fool thought to himself, 'The reason this food tastes so good is because of the salt. If a small amount is already this delicious, wouldn't it be even better if I ate a whole lot of it?'"
+    },
+    {
+        zh: "这个傻子没有智慧，于是就空口单独去吃盐。吃完之后他口味败坏，味觉麻木，盐反而成了他的祸害，让他痛苦不堪。",
+        py: "zhè gè shǎ zǐ méi yǒu zhì huì yú  shì jiù kōng kǒu dān dú qù chī yán chī wán zhī hòu tā kǒu wèi bài huài wèi jué má lù yán fǎn ér chéng le tā de huò hài ràng tā tòng kǔ bù kān",
+        en: "Lacking all common sense, the fool then ate the salt directly by itself. After eating it, his taste buds were completely ruined, and the salt instead became a painful disaster for him."
+    }
+];
